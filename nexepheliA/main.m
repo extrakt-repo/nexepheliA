260e650c18641de8aea1eeafbbb7e8523b006895
Patrick Hart <patrickhart@loaner-2.local>
2014-04-03 07:25:32 -0500

    Initial Commit

---
+++ nexepheliA/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  nexepheliA
+//
+//  Created by Patrick Hart on 4/3/14.
+//  Copyright (c) 2014 exTrakt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "nexepheliAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([nexepheliAAppDelegate class]));
+    }
+}
---
+++ nexepheliA/nexepheliAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  nexepheliAAppDelegate.h
+//  nexepheliA
+//
+//  Created by Patrick Hart on 4/3/14.
+//  Copyright (c) 2014 exTrakt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface nexepheliAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
